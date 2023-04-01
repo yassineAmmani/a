2132b5edeecb8a64df05a47ced3559136653e56e
yassineAmmani <y.ammani@edu.umi.ac.ma>
2023-04-01 15:02:36 +0000

    firebase connected

---
+++ app/src/main/java/com/example/pfe2023/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.pfe2023
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+      /*  var btnnext = findViewById<Button>(R.id.tvbtn)
+        btnnext.setOnClickListener {
+            val intent = Intent(this,basic_login::class.java)
+            startActivity(intent)
+        }
+
+       */
+    }
+}
